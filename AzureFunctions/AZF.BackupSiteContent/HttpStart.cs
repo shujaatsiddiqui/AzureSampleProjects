5d1424f23861421c83e2191f32cb579b63155fb0
Shujaat Siddiqui <shujaat.siddiqui@fitnessintl.com>
2022-10-11 19:23:44 -0700

    Sample azure functions

---
+++ AzureFunctions/AZF.BackupSiteContent/HttpStart.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AZF.BackupSiteContent
+{
+    public static class HttpStart
+    {
+        [FunctionName("HttpStart")]
+        public static async Task<HttpResponseMessage> Run(
+            [HttpTrigger(AuthorizationLevel.Function, methods: "post", Route = "orchestrators/{functionName}")] HttpRequestMessage req,
+            [DurableClient] IDurableClient starter,
+            string functionName,
+            ILogger log)
+        {
+            try
+            {
+                // Function input comes from the request content.
+                object eventData = await req.Content.ReadAsAsync<object>();
+                string instanceId = await starter.StartNewAsync(functionName, eventData);
+
+                log.LogInformation($"Started orchestration with ID = '{instanceId}'.");
+
+                return starter.CreateCheckStatusResponse(req, instanceId);
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex.Message, ex);
+
+                throw;
+            }
+        }
+    }
+}
